messageconcept/PeopleSyncClient

Make sure we do not accidentally dump or log a password

app/src/main/java/com/messageconcept/peoplesyncclient/settings/RestrictionsProvider.kt
@@ -92,7 +92,7 @@ class RestrictionsProvider(
         val keys = appRestrictions.keySet()
         val table = TextTable("Setting", "Value")
         keys.forEach { key ->
-            val value = appRestrictions.get(key)
+            val value = SettingsUtils.filterPassword(key, appRestrictions.get(key))
             table.addLine(key, value)
         }
         writer.write(table.toString())

atomica-software/contactzilla-sync

app/src/main/kotlin/com/messageconcept/peoplesyncclient/settings/RestrictionsProvider.kt
@@ -103,7 +103,7 @@ class RestrictionsProvider @Inject constructor(
         val keys = restrictions.keySet()
         val table = TextTable("Setting", "Value")
         keys.forEach { key ->
-            val value = restrictions.get(key)
+            val value = SettingsUtils.filterPassword(key, restrictions.get(key))
             table.addLine(key, value)
         }
         writer.write(table.toString())